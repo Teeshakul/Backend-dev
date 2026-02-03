52980b9603b195441a44352fdbcc8fe9d661eca6
Teeshakul <teesha119@gmail.com>
2026-02-03 14:59:45 +0530

    [Teesha] added data file for express project

---
+++ ExpressProject/data.js
@@ -0,0 +1,28 @@
+const userData = [
+  {
+    id: 1,
+    name: "Teesha",
+    age: 21,
+    email: "sdjbgjksg@sddgfnfk"
+  },
+  {
+    id: 2,
+    name: "mahak",
+    age: 21,
+    email: "abcd@kfj"
+  },
+  {
+    id: 3,
+    name: "nandini",
+    age: 20,
+    email: "skdjgk@fhdg"
+  },
+  {
+    id: 4,
+    name: "paridhi",
+    age: 22,
+    email: "hsyfr@dhgf"
+  }
+];
+
+module.exports = { userData };
