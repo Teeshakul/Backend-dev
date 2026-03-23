38393635c40ff933fe64f2686dcf0845a41403b7
Teeshakul <teesha119@gmail.com>
2026-03-23 15:52:09 +0530

    [Teesha] Added MongoDBCrud files

---
+++ MongoDBCrud/model/UserSchema.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        require:true,
+        maxLength:[25,"Name must contain 25 char only"]
+    },
+    email:{
+        type:String,
+        require:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        require:true,
+        minLength:[8,"Password must conatin 8 char only"]
+    },
+    role:{
+        type:String,
+        enum:["user","admin"],
+        default:"user"
+    }
+})
+
+const user = mongoose.model("User",userSchema);
+export default user;
---
+++ MongoDBCrud/server.js
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+import connectDb from "./config/db.js";
+import express from "express";
+import userRoutes from "./routes/userRoutes.js";
+
+
+dotenv.config();
+connectDb();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+
+app.use("/api",userRoutes)
+
+ app.listen(port,()=>{
+    console.log("Server is running on port",port)
+ })
