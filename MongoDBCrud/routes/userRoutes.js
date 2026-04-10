9ec779734d5f1e923f3f36e13e7bcd740e7d5105
Teesha <219322852+Teeshakul@users.noreply.github.com>
2026-04-10 14:39:20 +0530

    Delete MongoDBCrud directory

--- MongoDBCrud/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getAllUsers } from "../controllers/userControllers.js";
-
-const router = express.Router();
-
-router.get("/user",getAllUsers)
-
-export default router;
- 
