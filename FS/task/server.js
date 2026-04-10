990060ff8da2a765aaa148fa05a647145da4c18b
Teesha <219322852+Teeshakul@users.noreply.github.com>
2026-04-10 14:38:56 +0530

    Delete FS directory

--- FS/task/server.js
+++
@@ -1,6 +0,0 @@
-const log = require('./app.js');
-
-console.log(log.writeLogFile("Server started"));
-console.log(log.appendLogFile("User logged in"));
-console.log(log.readLogFile());
-console.log(log.deleteLogFile());
