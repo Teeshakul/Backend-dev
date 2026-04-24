1f422fdbea44c105815cb6e322c2c16c212c783e
Teesha <219322852+Teeshakul@users.noreply.github.com>
2026-04-24 14:45:31 +0530

    Delete Enterprise directory

--- Enterprise/models/Data.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const DataSchema = new mongoose.Schema({
-  name: String,
-  value: Number
-});
-
-module.exports = mongoose.model("Data", DataSchema);
--- Enterprise/routes/data.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const controller = require("../controllers/dataController");
-
-router.get("/", controller.getAllData);
-router.get("/:id", controller.getDataById);
-
-module.exports = router;
